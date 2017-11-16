a5f2120ccf5b7615bba1c029dec0015dfe894944
Joshua Salazar <joshuaasalazar@gmail.com>
2017-11-16 10:53:41 -0500

    init commit

---
+++ Assets/Scripts/cardHover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class cardHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer enter " + gameObject.name);
+        this.
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("Pointer exit " + gameObject.name);
+    }
+}
---
+++ Assets/Scripts/dropZone.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class dropZone : MonoBehaviour, IDropHandler {
+
+	public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log("OnDrop to " + gameObject.name);
+
+        draggable d = eventData.pointerDrag.GetComponent<draggable>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("OnDrop to " + gameObject.name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log(eventData.pointerDrag.name + " was dropped onto " + gameObject.name);
+    }
+}
