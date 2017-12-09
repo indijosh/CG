00e3a6ee782c9dbf36f52d3583a776271731015c
Joshua Salazar <joshuaasalazar@gmail.com>
2017-12-09 09:18:26 -0500

    merge?
    test

---
+++ Assets/Scripts/cardDB.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardDB : MonoBehaviour {
+    public enum CardDetail { CARDNAME, HP, CARDDESCRIPTION, ACTIONS }
+}
